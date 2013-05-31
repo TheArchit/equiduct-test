c820d22de2f250d7cadd24fa87654128698bd1f1
nishalr <thearchit@hotmail.com>
2013-05-31 11:34:22 +0100

    Added docstrings description to exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -2,8 +2,13 @@
 
 from sys import argv
 
+""" Write a function that takes a list of integers (as arguments) and
+returns a response with the average (mean), total/sum of integers, the
+maximum and minimum values. Print the average to two decimal places. To
+make this a little more straightforward assume that input values are always
+integers. """
+
 def maxminavg(nums):
-    
     sumall = sum(set(nums))
     avg = round(float(sumall) / len(nums), 2)
 
