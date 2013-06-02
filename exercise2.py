c15c907b4db0ec3053a4109e5302b7d9dd961ad1
nishalr <thearchit@hotmail.com>
2013-06-02 13:41:17 +0100

    Removed redundant variable declarations and unneeded Set()

--- exercise2.py
+++ exercise2.py
@@ -9,14 +9,13 @@ make this a little more straightforward assume that input values are always
 integers. """
 
 def maxminavg(nums):
-    sumall = sum(set(nums))
+    sumall = sum(nums)
     avg = round(float(sumall) / len(nums), 2)
 
     return avg, sumall, max(nums), min(nums)
 
 def main():
-    nums = [ int(argv[x]) for x in range(1, len(argv)) ]
-    print maxminavg(nums)
+    print maxminavg(map(int, argv[1:]))
 
 if __name__ == '__main__':
     main()
